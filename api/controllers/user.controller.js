f0a391f0685c8449c9c04ba50e7f2a89edbcc727
PrestonNguyen2001 <prestonnguyen2001@gmail.com>
2024-04-23 00:23:53 -0400

    Add delete user functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -56,8 +56,7 @@ export const updateUser = async (req, res, next) => {
 };
 
 export const deleteUser = async (req, res, next) => {
-  // !req.user.isAdmin && 
-  if (req.user.id !== req.params.userId) {
+  if (!req.user.isAdmin && req.user.id !== req.params.userId) {
     return next(errorHandler(403, "You are not allowed to delete this user"));
   }
   try {
